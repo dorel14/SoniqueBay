26e9a1c0ab47183aa52eb078295464e525d98444
David Orel <david.orel@sfr.fr>
2025-04-26 19:01:26 +0200

    Ajout de la gestion de version dans main.py, mise à jour des dépendances dans pyproject.toml et création du fichier _version.py.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from router import Router
 from nicegui import ui
+from ._version import version
 
 
 # Import de toutes les pages
 from ui.pages import accueil, recherche
 
-ui.run(title='SoniqueBay',
+ui.run(title=f'SoniqueBay v{version}',
         reload=True,
         favicon='https://emojiapi.dev/api/v1/headphones/64.png')
