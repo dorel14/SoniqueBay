5956b931c4c377d0c97da28947b14ab0165e6434
David Orel <david.orel@sfr.fr>
2025-04-28 12:46:03 +0200

    Nettoyage du code : suppression d'importations inutilisées et mise à jour de la configuration de version dans pyproject.toml pour une meilleure gestion des versions de développement.

--- backend/music_scan.py
+++ backend/music_scan.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import os
-from annotated_types import T
 from mutagen import File
 from .helpers.logging import logger
 
--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from nicegui import ui, app
 from src.soniquebay_app._version_ import version
 from backend.api.scan_api import router as scan_router
 
-
-
 app.include_router(scan_router, prefix='/api', tags=['scan'])  # Ajout du routeur d'API
-# Import de toutes les pages
-from ui.pages import accueil, recherche
 app.add_static_files('/static', './ui/static')
 
 
